ae606743996f6abd8bbafe71e26ba5de628e9715
RodrigoCSolari <rodrigocarlossolari@gmail.com>
2022-08-18 13:54:31 -0300

    feat: add dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Main from "./components/Main/Main";
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <div>tip-tap-toe</div>;
+  return (
+    <ChakraProvider resetCSS>
+      <QueryClientProvider client={queryClient}>
+        <Main />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ChakraProvider>
+  );
 }
 
 export default App;
